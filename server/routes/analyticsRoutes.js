1d136a87a43952f7b8c0275590d2e4ac632b2585
Mrunmayee Kokitkar <mrunmayee.kokitkar@gmail.com>
2026-07-08 11:43:23 +0530

    fix: Use correct named export for rate limiter

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { getAnalytics } from "../controllers/analyticsController.js";
-import rateLimiter from "../middleware/rateLimiter.js";
+import { apiLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
-router.get("/", rateLimiter, getAnalytics);
+router.get("/", apiLimiter, getAnalytics);
 
 export default router;
